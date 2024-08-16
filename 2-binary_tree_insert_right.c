2417b088a25da23de976f605c50a79b3381ffcb5
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-15 19:57:52 -0500

    Added missing ;

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,7 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
     new_node->n = value;
     new_node->parent = parent;
     new_node->right = parent->right;
-    new_node->left = NULL
+    new_node->left = NULL;
 
     if (parent->right != NULL)
     {
