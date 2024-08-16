c62fe4a125d0f8dd987c4f95b8cb56ae8a8af95d
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-15 19:53:37 -0500

    Create file 2, add program

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+    /* Create variable for new node */
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+
+    new_node = malloc(sizeof(binary_tree_t));
+    /* Set aside memory for new node */
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+    /* Error handling if malloc fails */
+
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->right = parent->right;
+    new_node->left = NULL
+
+    if (parent->right != NULL)
+    {
+        parent->right->parent = new_node;
+    }
+    parent->right = new_node;
+
+    return (new_node);
+}
